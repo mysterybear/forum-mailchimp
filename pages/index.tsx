337c82759428f4a7cdaa6db31f240c3448f1d911
Thomas O'Neill <tom@opensystemslab.io>
2021-12-03 16:41:30 +0000

    -

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,22 @@
-import React from 'react'
-import tw, { styled } from 'twin.macro'
-import { Logo, Button } from '../components'
+import React from "react"
 
-const Container = styled.div({
-  ...tw`flex flex-col items-center justify-center h-screen`,
-  variants: {
-    hasBackground: {
-      true: tw`bg-gradient-to-b from-electric to-ribbon`,
-    },
-  },
-})
+// const Container = styled.div({
+//   ...tw`flex flex-col items-center justify-center h-screen`,
+//   variants: {
+//     hasBackground: {
+//       true: tw`bg-gradient-to-b from-electric to-ribbon`,
+//     },
+//   },
+// })
 
-const ButtonBox = tw.div`flex flex-col justify-center h-full gap-y-5`
+// const ButtonBox = tw.div`flex flex-col justify-center h-full gap-y-5`
 
-const IndexPage = () => (
-  <Container hasBackground>
-    <ButtonBox>
-      <Button variant="primary">Submit</Button>
-      <Button variant="secondary">Cancel</Button>
-      <Button isSmall>Close</Button>
-    </ButtonBox>
-    <Logo />
-  </Container>
-)
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Just give me the markup :)</h1>
+    </div>
+  )
+}
 
 export default IndexPage
